9adc0914c32173a2aa93951d3540a8bb1bfd5eec
emilie-jin <emilie.jin@hotmail.com>
2016-04-27 14:30:36 +0200

    add controller
    page réponse ajouter controller pour obtenir les réponses

--- app/scripts/controllers/reponse.js
+++ app/scripts/controllers/reponse.js
@@ -8,20 +8,14 @@
  * Controller of the projetsEpsiApp
  */
 angular.module('projetsEpsiApp')
-  .controller('MainCtrl', function ($scope,$http,$location) {
+  .controller('ReponseCtrl', function ($scope,$http,$location) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
-	$scope.email = {
-	};
-	$scope.list = [''];
-	$scope.emailSend = function(){
-			if ($scope.email.votroMail) {
-	          $scope.email.push($scope.email.votroMail);
 
-	        }
-	}
+	$scope.reponse = [];
+	
   });
